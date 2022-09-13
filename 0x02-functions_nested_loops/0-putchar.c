9e3ef7ae381e06ffdc7f6093e9526d3c385678ab
Ochuko-west <oogheneare@gmail.com>
2022-09-13 11:07:37 -0700

    fix linking issues with header files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 /**
 *  main - prints keyword _putchar
@@ -12,9 +11,8 @@ int main(void)
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch = 0; ch < 8; ++ch)
-		putchar(str[ch]);
-		putchar('\n');
-
+for (ch = 0; ch < 8; ++ch)
+	{_putchar(str[ch]);	}
+_putchar('\n');
 	return (0);
 }
