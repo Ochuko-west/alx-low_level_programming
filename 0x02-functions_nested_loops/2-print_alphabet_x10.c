f3b413c5e7b340911dc6a54cc57e05954527e58c
Ochuko-west <oogheneare@gmail.com>
2022-09-13 17:49:40 -0700

    resolved checker errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,17 @@
  * Return: void
  */
 void print_alphabet_x10(void)
-{	char ch; 
+{	char ch;
 	int i = 0;
-	while ( i < 10)
-	{	
-		for(ch = 'a'; ch <= 'z'; ch++)
-		{_putchar(ch);}
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch)
+		}
 		_putchar('\n');
-	i++;
+		i++;
 	}
 }
 
